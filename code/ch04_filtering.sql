f92e7b696a06e93b66445fb0f8f8a2db65a943dc
atta <atta_ub@hotmail.com>
2025-06-17 20:37:41 -0700

    minor changes

--- code/ch04_filtering.sql
+++ code/ch04_filtering.sql
@@ -29,92 +29,36 @@ ON p.product_type_cd = pt.product_type_cd
 WHERE pt.name <> 'Customer Accounts';
 -- WHERE pt.name != 'Customer Accounts';
 
-DELETE FROM account
-  WHERE status = 'CLOSED' AND YEAR(close_date) = 2002;
+DELETE FROM account WHERE status = 'CLOSED' AND YEAR(close_date) = 2002;
 
-SELECT emp_id, fname, lname, start_date
- FROM employee
- WHERE start_date < '2007-01-01';
+SELECT emp_id, fname, lname, start_date FROM employee WHERE start_date < '2007-01-01';
+SELECT emp_id, fname, lname, start_date FROM employee WHERE start_date < '2007-01-01' AND start_date >= '2005-01-01';
 
-SELECT emp_id, fname, lname, start_date
- FROM employee
- WHERE start_date < '2007-01-01'
-AND start_date >= '2005-01-01';
+SELECT emp_id, fname, lname, start_date FROM employee WHERE start_date BETWEEN '2005-01-01' AND '2007-01-01';
+SELECT emp_id, fname, lname, start_date FROM employee WHERE start_date BETWEEN '2007-01-01' AND '2005-01-01'; 
+SELECT account_id, product_cd, cust_id, avail_balance FROM account WHERE avail_balance BETWEEN 3000 AND 5000;
 
-SELECT emp_id, fname, lname, start_date
- FROM employee
- WHERE start_date BETWEEN '2005-01-01' AND '2007-01-01';
+SELECT cust_id, fed_id FROM customer WHERE cust_type_cd = 'I' AND fed_id BETWEEN '500-00-0000' AND '999-99-9999'; 
+-- SELECT cust_id, fed_id, cust_type_cd FROM customer WHERE cust_type_cd = 'I' AND fed_id BETWEEN '500-00-0000' AND '999-99-9999';
 
-SELECT emp_id, fname, lname, start_date
- FROM employee
- WHERE start_date BETWEEN '2007-01-01' AND '2005-01-01';
+SELECT account_id, product_cd, cust_id, avail_balance FROM account WHERE product_cd = 'CHK' OR product_cd = 'SAV' OR product_cd = 'CD' OR product_cd = 'MM';
+SELECT account_id, product_cd, cust_id, avail_balance FROM account WHERE product_cd IN ('CHK','SAV','CD','MM');
 
-SELECT account_id, product_cd, cust_id, avail_balance
- FROM account
- WHERE avail_balance BETWEEN 3000 AND 5000;
+SELECT account_id, product_cd, cust_id, avail_balance FROM account WHERE product_cd IN (SELECT product_cd FROM product WHERE product_type_cd = 'ACCOUNT');
+SELECT account_id, product_cd, cust_id, avail_balance FROM account WHERE product_cd NOT IN ('CHK','SAV','CD','MM');
 
-SELECT cust_id, fed_id
- FROM customer
- WHERE cust_type_cd = 'I'
-AND fed_id BETWEEN '500-00-0000' AND '999-99-9999';
+SELECT emp_id, fname, lname FROM employee WHERE LEFT(lname, 1) = 'T';
 
-SELECT account_id, product_cd, cust_id, avail_balance
- FROM account
- WHERE product_cd = 'CHK' OR product_cd = 'SAV'
-OR product_cd = 'CD' OR product_cd = 'MM';
+SELECT lname FROM employee WHERE lname LIKE '_a%e%'; 
+SELECT cust_id, fed_id FROM customer WHERE fed_id LIKE '___-__-____'; 
+SELECT emp_id, fname, lname FROM employee WHERE lname LIKE 'F%' OR lname LIKE 'G%';
+SELECT emp_id, fname, lname FROM employee WHERE lname REGEXP '^[FG]';
 
-SELECT account_id, product_cd, cust_id, avail_balance
-FROM account
-WHERE product_cd IN ('CHK','SAV','CD','MM');
-
-SELECT account_id, product_cd, cust_id, avail_balance
- FROM account
- WHERE product_cd IN (SELECT product_cd FROM product
-WHERE product_type_cd = 'ACCOUNT');
-
-SELECT account_id, product_cd, cust_id, avail_balance
-FROM account
-WHERE product_cd NOT IN ('CHK','SAV','CD','MM');
-
-SELECT emp_id, fname, lname
- FROM employee
- WHERE LEFT(lname, 1) = 'T';
-
-SELECT lname
- FROM employee
- WHERE lname LIKE '_a%e%';
-
-SELECT cust_id, fed_id
- FROM customer
- WHERE fed_id LIKE '___-__-____';
-
-SELECT emp_id, fname, lname
-FROM employee
-WHERE lname LIKE 'F%' OR lname LIKE 'G%';
-
-SELECT emp_id, fname, lname
- FROM employee
- WHERE lname REGEXP '^[FG]';
-
-SELECT emp_id, fname, lname, superior_emp_id
- FROM employee
- WHERE superior_emp_id IS NULL;
-
-SELECT emp_id, fname, lname, superior_emp_id 
-  FROM employee
-  WHERE superior_emp_id = NULL;
-
-SELECT emp_id, fname, lname, superior_emp_id
- FROM employee
- WHERE superior_emp_id IS NOT NULL;
-
-SELECT emp_id, fname, lname, superior_emp_id
- FROM employee
- WHERE superior_emp_id != 6;
-
-SELECT emp_id, fname, lname, superior_emp_id
-FROM employee
-WHERE superior_emp_id != 6 OR superior_emp_id IS NULL;
+SELECT emp_id, fname, lname, superior_emp_id FROM employee WHERE superior_emp_id IS NULL;
+SELECT emp_id, fname, lname, superior_emp_id FROM employee WHERE superior_emp_id = NULL;
+SELECT emp_id, fname, lname, superior_emp_id FROM employee WHERE superior_emp_id IS NOT NULL; 
+SELECT emp_id, fname, lname, superior_emp_id FROM employee WHERE superior_emp_id != 6; 
+SELECT emp_id, fname, lname, superior_emp_id FROM employee WHERE superior_emp_id != 6 OR superior_emp_id IS NULL;
 
 txn_date < '2005-02-26' AND (txn_type_cd = 'DBT' OR amount > 100)
 
--- code/ch05_querying_multiple_tables.sql
+++ code/ch05_querying_multiple_tables.sql
@@ -37,38 +37,29 @@ ON e.assigned_branch_id = b.branch_id
 AND (e.title = 'Teller' OR e.title = 'Head Teller')
 AND b.name = 'Woburn Branch';
 
-SELECT a.account_id, c.fed_id
-FROM account a INNER JOIN customer c
-ON a.cust_id = c.cust_id
-WHERE c.cust_type_cd = 'B';
+SELECT a.account_id, c.fed_id FROM account a
+  INNER JOIN customer c ON a.cust_id = c.cust_id
+    WHERE c.cust_type_cd = 'B';
 
-SELECT a.account_id, c.fed_id, e.fname, e.lname
-  FROM account a INNER JOIN customer c
-ON a.cust_id = c.cust_id
-INNER JOIN employee e
-ON a.open_emp_id = e.emp_id
-  WHERE c.cust_type_cd = 'B';
+SELECT a.account_id, c.fed_id, e.fname, e.lname FROM account a
+  INNER JOIN customer c ON a.cust_id = c.cust_id
+  INNER JOIN employee e ON a.open_emp_id = e.emp_id
+    WHERE c.cust_type_cd = 'B'; 
 
-SELECT a.account_id, c.fed_id, e.fname, e.lname
- FROM customer c INNER JOIN account a
- ON a.cust_id = c.cust_id
- INNER JOIN employee e
- ON a.open_emp_id = e.emp_id
- WHERE c.cust_type_cd = 'B';
+SELECT a.account_id, c.fed_id, e.fname, e.lname FROM customer c
+  INNER JOIN account a ON a.cust_id = c.cust_id
+  INNER JOIN employee e ON a.open_emp_id = e.emp_id
+    WHERE c.cust_type_cd = 'B'; 
+    
+SELECT a.account_id, c.fed_id, e.fname, e.lname FROM employee e
+  INNER JOIN account a ON e.emp_id = a.open_emp_id
+  INNER JOIN customer c ON a.cust_id = c.cust_id
+   WHERE c.cust_type_cd = 'B'; 
 
-SELECT a.account_id, c.fed_id, e.fname, e.lname
- FROM employee e INNER JOIN account a
- ON e.emp_id = a.open_emp_id
- INNER JOIN customer c
- ON a.cust_id = c.cust_id
- WHERE c.cust_type_cd = 'B';
-
-SELECT STRAIGHT_JOIN a.account_id, c.fed_id, e.fname, e.lname
-FROM customer c INNER JOIN account a
-ON a.cust_id = c.cust_id
-INNER JOIN employee e
-ON a.open_emp_id = e.emp_id
-WHERE c.cust_type_cd = 'B';
+SELECT STRAIGHT_JOIN a.account_id, c.fed_id, e.fname, e.lname FROM customer c
+  INNER JOIN account a ON a.cust_id = c.cust_id
+  INNER JOIN employee e ON a.open_emp_id = e.emp_id
+    WHERE c.cust_type_cd = 'B';
 
 SELECT a.account_id, a.cust_id, a.open_date, a.product_cd
   FROM account a INNER JOIN
@@ -102,48 +93,38 @@ INNER JOIN branch b_e
 ON e.assigned_branch_id = b_e.branch_id
 WHERE a.product_cd = 'CHK';
 
-SELECT e.fname, e.lname,
-e_mgr.fname mgr_fname, e_mgr.lname mgr_lname
-FROM employee e INNER JOIN employee e_mgr
-ON e.superior_emp_id = e_mgr.emp_id;
+SELECT e.fname, e.lname, e_mgr.fname mgr_fname, e_mgr.lname mgr_lname FROM employee e
+  INNER JOIN employee e_mgr ON e.superior_emp_id = e_mgr.emp_id;
 
--- ON e.assigned_branch_id = b.branch_id
+-- ON e.assigned_branch_id = b.branch_id 
+SELECT e.emp_id, e.fname, e.lname, e.start_date FROM employee e
+  INNER JOIN product p ON e.start_date >= p.date_offered AND e.start_date <= p.date_retired
+  WHERE p.name = 'no-fee checking'; 
 
-SELECT e.emp_id, e.fname, e.lname, e.start_date
-FROM employee e INNER JOIN product p
-ON e.start_date >= p.date_offered
-AND e.start_date <= p.date_retired
-WHERE p.name = 'no-fee checking';
+SELECT e1.fname, e1.lname, 'VS' vs, e2.fname, e2.lname FROM employee e1
+  INNER JOIN employee e2 ON e1.emp_id != e2.emp_id
+  WHERE e1.title = 'Teller' AND e2.title = 'Teller'; 
 
-SELECT e1.fname, e1.lname, 'VS' vs, e2.fname, e2.lname
-FROM employee e1 INNER JOIN employee e2
-ON e1.emp_id != e2.emp_id
-WHERE e1.title = 'Teller' AND e2.title = 'Teller';
+SELECT e1.fname, e1.lname, 'VS' vs, e2.fname, e2.lname FROM employee e1
+  INNER JOIN employee e2 ON e1.emp_id < e2.emp_id
+  WHERE e1.title = 'Teller' AND e2.title = 'Teller';
 
-SELECT e1.fname, e1.lname, 'VS' vs, e2.fname, e2.lname
-FROM employee e1 INNER JOIN employee e2
-ON e1.emp_id < e2.emp_id
-WHERE e1.title = 'Teller' AND e2.title = 'Teller';
+SELECT a.account_id, a.product_cd, c.fed_id FROM account a
+  INNER JOIN customer c ON a.cust_id = c.cust_id
+  WHERE c.cust_type_cd = 'B'; 
 
-SELECT a.account_id, a.product_cd, c.fed_id
- FROM account a INNER JOIN customer c
-ON a.cust_id = c.cust_id
- WHERE c.cust_type_cd = 'B';
+SELECT a.account_id, a.product_cd, c.fed_id FROM account a
+  INNER JOIN customer c ON a.cust_id = c.cust_id
+  AND c.cust_type_cd = 'B'; 
 
-SELECT a.account_id, a.product_cd, c.fed_id
-FROM account a INNER JOIN customer c
-ON a.cust_id = c.cust_id
-AND c.cust_type_cd = 'B';
+SELECT a.account_id, a.product_cd, c.fed_id FROM account a
+  INNER JOIN customer c WHERE a.cust_id = c.cust_id AND c.cust_type_cd = 'B';
 
-SELECT a.account_id, a.product_cd, c.fed_id
-FROM account a INNER JOIN customer c
-WHERE a.cust_id = c.cust_id
-AND c.cust_type_cd = 'B';
+SELECT e.emp_id, e.fname, e.lname, b.name FROM employee e INNER JOIN <1> b ON e.assigned_branch_id = b.<2>; #+END_SRC 
+SELECT e.emp_id, e.fname, e.lname, b.name FROM employee e INNER JOIN branch b ON e.assigned_branch_id = b.branch_id;
+SELECT e.emp_id, e.fname, e.lname, b.name FROM employee e INNER JOIN <1> b ON e.assigned_branch_id = b.<2>;
+SELECT e.emp_id, e.fname, e.lname, b.name FROM employee e INNER JOIN branch b ON e.assigned_branch_id = b.branch_id;
 
-mysql> SELECT e.emp_id, e.fname, e.lname, b.name
-FROM employee e INNER JOIN <1> b
-ON e.assigned_branch_id = b.<2>;
-
-SELECT e.emp_id, e.fname, e.lname, b.name
-  FROM employee e INNER JOIN <1> b
-  ON e.assigned_branch_id = b.<2>;
+SELECT a.account_id, c.fed_id from account a    
+INNER JOIN  customer c ON a.cust_id = c.cust_id
+WHERE c.cust_type_cd = 'I';
